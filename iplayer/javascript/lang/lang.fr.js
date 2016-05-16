8ebe6bcb86a0714adfc8607c5bf946ff9c78a1de
elyseeFR <root@ns238050.ip-5-39-64.eu>
2016-05-16 19:24:57 +0200

    -Multilang support.

---
+++ iplayer/javascript/lang/lang.fr.js
@@ -0,0 +1,13 @@
+iplayer_langs['fr'] = {
+    'CLOSE': 'Fermer',
+    'REPLAY': 'Rejouer cette vidéo',
+    'SHARE_VIDEO': 'Partager cette vidéo',
+    'SHARE_CONTENT': 'Partager ce contenu',
+    'EXPORT_VIDEO': 'Exporter cette vidéo',
+    'SHARE_FB': 'Partager sur Facebook',
+    'SHARE_TW': 'Partager sur Twitter',
+    'RETURN_MAIN_VIDEO': 'Retour à la vidéo principale',
+    'EXPORT_WIDTH': 'Largeur',
+    'EXPORT_AUTOPLAY': 'Démarrer automatiquement',
+    'LOADING_WAIT': 'Chargement de la vidéo en cours...'
+};
